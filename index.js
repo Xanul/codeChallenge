be28f8a85ef560761f05ae5f1884558e53a4fc23
Xanul <rivas.060@gmail.com>
2022-05-05 23:28:52 -0500

    Applied ESLint to the project

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const StudentController = require("./lib/controllers/StudentController");
 
 
-const test = StudentController.getEmailWithCertificationCtrl()
-console.log(test)
+const test = StudentController.getEmailWithCertificationCtrl();
+console.log(test);
 
 
 
--- test/services/StudentService.test.js
+++ test/services/StudentService.test.js
@@ -2,32 +2,32 @@ const StudentService = require("../../lib/services/StudentService");
 const Reader = require("../../lib/utils/reader");
 
 
-const allStudentsFile = Reader.readDataFile('./test/data/studentsTest.json');
+const allStudentsFile = Reader.readDataFile("./test/data/studentsTest.json");
 
 
-describe('Testing StudentService.js', () => {
+describe("Testing StudentService.js", () => {
     
-    test('Testing getAllStudents method', () => {
+    test("Testing getAllStudents method", () => {
         const allStudents = StudentService.getAllStudents(allStudentsFile);
         expect(allStudents.length).toBe(51);
-        expect(allStudents[0]).toBeDefined()
-    })
+        expect(allStudents[0]).toBeDefined();
+    });
 
-    test('Testing getEmailWithCertification method', () => {        
+    test("Testing getEmailWithCertification method", () => {        
         const certifiedStudentsEmails = StudentService.getEmailWithCertification(allStudentsFile);
         expect(certifiedStudentsEmails.length).toBe(29);
         
         certifiedStudentsEmails.forEach((student) => {
-            expect(student).toContain('@')
-            expect(student).toMatch(/.xyz/)
-        })
-    })
+            expect(student).toContain("@");
+            expect(student).toMatch(/.xyz/);
+        });
+    });
 
-    test('Testing getStudentsTopCredits method', () => {        
+    test("Testing getStudentsTopCredits method", () => {        
         const topScoreStudents = StudentService.getStudentsTopCredits(allStudentsFile);
         topScoreStudents.forEach((student)=> {
-            expect(student).toBeDefined()
+            expect(student).toBeDefined();
             expect(student.credits).toBeGreaterThan(500);
-        })
-    })
-})
+        });
+    });
+});
